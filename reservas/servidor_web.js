370c9c0813d7c1f40e38debab02e4fbae63a927c
JoaoVTeixeira <103959681+JoaoVTeixeira@users.noreply.github.com>
2022-12-12 16:50:52 -0400

    zlkjaslkdjads

--- reservas/servidor_web.js
+++ reservas/servidor_web.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(function(request, response){
-response.writeHead(200, {"Content-Type": "text/html"});
-response.write("<h1>Hello World!</h1>");
+// A constante __dirname retorna o diretório raiz da aplicação.
+fs.readFile('condominio/index.html', function(err, html){
+
+response.write(html);
 response.end();
 });
-server.listen(3000);
+});
+server.listen(3000, function(){
+console.log('Executando Site Pessoal');
+});
